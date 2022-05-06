76d5b8341269a67a8e2c0599fa12abfbfd8c3421
Tahsin Taydaş <56533800+taydastahsin@users.noreply.github.com>
2022-05-06 15:30:36 +0300

    06.05.2022

---
+++ flutter_basit_hesap_makinesi_app/android/app/src/main/kotlin/com/example/flutter_basit_hesap_makinesi_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_basit_hesap_makinesi_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_basit_hesap_makinesi_app/lib/main.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Hesap Makinesi",
+      home: Iskele(),
+    );
+  }
+}
+
+class Iskele extends StatelessWidget {
+  const Iskele({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Hesap Makinesi APP")),
+      body: AnaEkran(),
+    );
+  }
+}
+
+class AnaEkran extends StatefulWidget {
+  const AnaEkran({Key? key}) : super(key: key);
+
+  @override
+  State<AnaEkran> createState() => _AnaEkranState();
+}
+
+class _AnaEkranState extends State<AnaEkran> {
+  num sayi1 = 0, sayi2 = 0, sonuc = 0;
+
+  TextEditingController t1 = TextEditingController();
+  TextEditingController t2 = TextEditingController();
+
+  topla() {
+    setState(() {
+      sayi1 = num.parse(t1.text);
+      sayi2 = num.parse(t2.text);
+      sonuc = sayi1 + sayi2;
+    });
+  }
+
+  cikar() {
+    setState(() {
+      sayi1 = num.parse(t1.text);
+      sayi2 = num.parse(t2.text);
+      sonuc = sayi1 - sayi2;
+    });
+  }
+
+  bolme() {
+    setState(() {
+      sayi1 = num.parse(t1.text);
+      sayi2 = num.parse(t2.text);
+      sonuc = sayi1 / sayi2;
+    });
+  }
+
+  carpma() {
+    setState(() {
+      sayi1 = num.parse(t1.text);
+      sayi2 = num.parse(t2.text);
+      sonuc = sayi1 * sayi2;
+    });
+  }
+
+  modKalan() {
+    setState(() {
+      sayi1 = num.parse(t1.text);
+      sayi2 = num.parse(t2.text);
+      sonuc = sayi1 % sayi2;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(50),
+      child: Center(
+        child: Column(
+          children: [
+            Text("$sonuc"),
+            TextField(
+              controller: t1,
+            ),
+            TextField(
+              controller: t2,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                RaisedButton(
+                  onPressed: topla,
+                  child: Text("Topla"),
+                ),
+                RaisedButton(
+                  onPressed: cikar,
+                  child: Text("Çıkar"),
+                ),
+                RaisedButton(
+                  onPressed: bolme,
+                  child: Text("Böl"),
+                ),
+                RaisedButton(
+                  onPressed: carpma,
+                  child: Text("Çarp"),
+                ),
+                RaisedButton(
+                  onPressed: modKalan,
+                  child: Text("Mod Al"),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
